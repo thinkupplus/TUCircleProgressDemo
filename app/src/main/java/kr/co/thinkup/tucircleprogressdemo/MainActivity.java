ccf77d3439ef13d153ab1f924d0f81bda544abac
yhhoi <thinkupplus@gmail.com>
2019-12-12 19:27:01 +0900

    gradition add

--- app/src/main/java/kr/co/thinkup/tucircleprogressdemo/MainActivity.java
+++ app/src/main/java/kr/co/thinkup/tucircleprogressdemo/MainActivity.java
@@ -2,7 +2,11 @@ package kr.co.thinkup.tucircleprogressdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
+import android.widget.SeekBar;
+
+import kr.co.thinkup.tucircleprogress.ArcProgress;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ArcProgress arcProgress = findViewById(R.id.arcprogress);
+        SeekBar seekBar = findViewById(R.id.seekbar);
+
+        seekBar.setProgress(10);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                arcProgress.setProgress(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
